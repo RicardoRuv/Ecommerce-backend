d3fd3ce4b2421a48a255fe7d347dd45ea249a6c8
RicardoRuv <75161937+RicardoRuv@users.noreply.github.com>
2023-12-18 10:55:15 -0700

    Added initial API - controller and auth directories

---
+++ src/main/java/com/runescape/demo/api/model/RegistrationBody.java
@@ -0,0 +1,10 @@
+package com.runescape.demo.api.model;
+
+public class RegistrationBody {
+
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+}
